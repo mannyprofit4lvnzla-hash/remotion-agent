a40268c770797648398e32d6feda00550ca7df21
Manuel Alvarado <manuelalvarado@Manuels-MacBook-Pro.local>
2026-02-18 22:20:20 -0500

    fix: remove unsupported setDelayRenderTimeoutInSeconds config

--- Remotion_Video_Agent/remotion/remotion.config.ts
+++ Remotion_Video_Agent/remotion/remotion.config.ts
@@ -2,5 +2,4 @@ import { Config } from '@remotion/cli/config';
 
 Config.setVideoImageFormat('jpeg');
 Config.setOverwriteOutput(true);
-Config.setDelayRenderTimeoutInSeconds(120); // Increase timeout to 2 minutes
 Config.setConcurrency(1); // To save memory on small instances
