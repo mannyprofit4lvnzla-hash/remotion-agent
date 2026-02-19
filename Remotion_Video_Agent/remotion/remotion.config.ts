5ed239271ba8c533ffbd4625a117fa88f8fadec7
Manuel Alvarado <manuelalvarado@Manuels-MacBook-Pro.local>
2026-02-18 22:24:43 -0500

    fix: align remotion versions and increase timeout

--- Remotion_Video_Agent/remotion/remotion.config.ts
+++ Remotion_Video_Agent/remotion/remotion.config.ts
@@ -2,4 +2,5 @@ import { Config } from '@remotion/cli/config';
 
 Config.setVideoImageFormat('jpeg');
 Config.setOverwriteOutput(true);
+Config.setDelayRenderTimeoutInSeconds(120);
 Config.setConcurrency(1); // To save memory on small instances
