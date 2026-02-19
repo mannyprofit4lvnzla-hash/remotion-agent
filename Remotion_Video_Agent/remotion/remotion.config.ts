4fa01ff621848ca89ed93991a3b86d8dd5a0b0c2
Manuel Alvarado <manuelalvarado@Manuels-MacBook-Pro.local>
2026-02-18 22:07:17 -0500

    fix: increase remotion timeout to 120s

--- Remotion_Video_Agent/remotion/remotion.config.ts
+++ Remotion_Video_Agent/remotion/remotion.config.ts
@@ -2,4 +2,5 @@ import { Config } from '@remotion/cli/config';
 
 Config.setVideoImageFormat('jpeg');
 Config.setOverwriteOutput(true);
+Config.setDelayRenderTimeoutInSeconds(120); // Increase timeout to 2 minutes
 Config.setConcurrency(1); // To save memory on small instances
